7f25ccd9719783c36d97e3894a3be687981ad200
Sean Kelliher <sbkelliher@icloud.com>
2024-02-25 10:02:16 -0500

    Remove figcaption. Add class 'pano-title' to position title on 'pano' class.

--- src/components/ImagePano.vue
+++ src/components/ImagePano.vue
@@ -6,6 +6,7 @@
         <figure class="full-width"> <!-- RESIZE IMAGE - CURRENTLY HUGE. -->
             <img src="/gettysburg.jpg" alt="add description">
         </figure>
+        <div class="pano-title">Park Alerts</div>
     </div>
 </template>
 
