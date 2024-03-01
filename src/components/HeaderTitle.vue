57786de0d67028df01566483284ada2cab939f94
Sean Kelliher <sbkelliher@icloud.com>
2024-03-01 15:30:16 -0500

    Add figcaption for accessibility.

--- src/components/HeaderTitle.vue
+++ src/components/HeaderTitle.vue
@@ -11,6 +11,7 @@ defineProps({
     <header>
         <figure class="logo">
             <img src="/national-park-service-logo.svg" alt="National Park Service logo">
+            <figcaption hidden>National Park Service logo</figcaption>
         </figure>
         <h1>{{ title }}</h1>
     </header>
