a0acc9a19325f0cd47f620796e5a9265c0e968a1
Sean Kelliher <sbkelliher@icloud.com>
2024-03-01 15:31:00 -0500

    Modify alt. Add figcaption for acessibility.

--- src/components/ImagePano.vue
+++ src/components/ImagePano.vue
@@ -6,7 +6,9 @@
         <figure class="full-width">
             <img
                 src="/gettysburg-battlefield.jpg"
-                alt="First Day's Battlefield, Gettysburg. Photo: NPS/Mary O'Neill.">
+                alt="First Day's Battlefield, Gettysburg. Photo: NPS/Mary O'Neill."
+            >
+            <figcaption hidden>Decorative photo of battlefield at Gettysburg, present day</figcaption>
         </figure>
         <div class="pano-title">Park Alerts</div>
     </div>
