58de1bed5d8a8959c2b5098e1cb4f4f7f03183fe
Sean Kelliher <sbkelliher@icloud.com>
2024-08-13 23:12:55 -0400

    Add component FooterNotes.vue.

---
+++ src/components/FooterNotes.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <footer>
+        <p class="white-txt">National Park Service<br />1849 C Street NW<br />Washington, DC 20240</p>
+    </footer>
+</template>
+
+<style scoped>
+</style>
