d2ebfdd6c50322d69214814dd53ebf6702c62387
Sean Kelliher <sbkelliher@icloud.com>
2024-08-13 23:16:26 -0400

    Add LookupBox and FooterNotes components.

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,18 @@ import HeaderTitle from "./components/HeaderTitle.vue";
 import ImagePano from "./components/ImagePano.vue";
 import OverviewBoxes from "./components/OverviewBoxes.vue";
 import MainComponent from "./components/MainComponent.vue";
+import LookupBox from "./components/LookupBox.vue";
+import FooterNotes from "./components/FooterNotes.vue";
 </script>
 
 <template>
     <div id="container">
         <HeaderTitle title="National Park Service" />
         <ImagePano />
-        <MainComponent />
         <OverviewBoxes />
+        <MainComponent />
+        <LookupBox />
+        <FooterNotes />
     </div>
 </template>
 
