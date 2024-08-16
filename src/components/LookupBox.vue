79cb68df9924b6972a43f1837d3fa106a093cd15
Sean Kelliher <sbkelliher@icloud.com>
2024-08-16 16:05:05 -0400

    Add 'role-listbox' to lookup-results class.

--- src/components/LookupBox.vue
+++ src/components/LookupBox.vue
@@ -268,7 +268,7 @@ function readyClip() {
                 Z
             </span>
         </div>
-        <div class="lookup-results" tabindex="0">
+        <div class="lookup-results" tabindex="0" role="listbox">
             <ul>
                 <li v-for="select in selects.sort()" :key="select.index">
                     <span class="copy-name">{{ select }}</span>
